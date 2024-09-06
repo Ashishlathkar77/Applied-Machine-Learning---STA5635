e6bafdc02984b8193342c90ece1afd927970bdb8
Ashish Lathkar <63287421+Ashishlathkar77@users.noreply.github.com>
2024-09-05 23:40:18 -0400

    Update Question2.py

--- Homework 1/Question2.py
+++ Homework 1/Question2.py
@@ -1,18 +1,25 @@
-train_data_path = '/content/Satimage/X.dat'
-test_data_path = '/content/Satimage/Xtest.dat'
-train_labels_path = '/content/Satimage/Y.dat'
-test_labels_path = '/content/Satimage/Ytest.dat'
+satimage_train_data_path = '/content/Satimage/X.dat'
+satimage_test_data_path = '/content/Satimage/Xtest.dat'
+satimage_train_labels_path = '/content/Satimage/Y.dat'
+satimage_test_labels_path = '/content/Satimage/Ytest.dat'
 
-max_depth = 12
 
-X_train, X_test, Y_train, Y_test = load_data(train_data_path, test_data_path, train_labels_path, test_labels_path)
+def load_data(train_path, test_path, train_labels_path, test_labels_path):
+    X_train = np.loadtxt(train_path)
+    X_test = np.loadtxt(test_path)
+    Y_train = np.loadtxt(train_labels_path)
+    Y_test = np.loadtxt(test_labels_path)
+    return X_train, X_test, Y_train, Y_test
 
-train_errors, test_errors = calculate_errors(X_train, Y_train, X_test, Y_test, max_depth)
 
-optimal_tree_depth, minimum_test_error = find_optimal_depth(test_errors)
+if __name__ == "__main__":
+    X_train, X_test, Y_train, Y_test = load_data(satimage_train_data_path, satimage_test_data_path, satimage_train_labels_path, satimage_test_labels_path)
 
-plot_errors(train_errors, test_errors, max_depth)
 
-print(f'Optimal Tree Depth: {optimal_tree_depth}')
-print(f'Minimum Test Error: {minimum_test_error:.4f}')
-print_error_summary(train_errors, test_errors)
+    max_depth = 12
+    tr_errs, te_errs = calc_dt_errors(X_train, Y_train, X_test, Y_test, max_depth)
+    best_depth, min_te_err = find_optimal_depth(te_errs)
+    plot_errors(range(1, max_depth + 1), tr_errs, te_errs, 'Decision Tree Errors vs Depth (Satimage)')
+    print(f'Optimal Tree Depth: {best_depth}')
+    print(f'Minimum Test Error: {min_te_err:.4f}')
+    print_error_summary(range(1, max_depth + 1), tr_errs, te_errs)
